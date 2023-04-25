9d4205c2324095f74e8fc595e2a4d9cb668033ce
IBTISSAMELBOUDALI <ibtissamelboudali@gmail.com>
2023-04-25 06:07:21 -0700

    0x11. C - printf

--- get_precision.c
+++ get_precision.c
@@ -13,16 +13,16 @@ int *get_precision(char *p, params_t *params, va_list ap)
 	int d = 0;
 
 	if (*p != '.')
-		return (p)
+		return (p);
 	p++
-	if ($p == '*')
+	if (*p == '*')
 	{
 		d = va_arg(ap, int);
 		p++
 	}
 	else
 	{
-		while (_isdigit (*p))
+		while (_isdigit(*p))
 			d = d * 10 + (*p++ - '0';
 	}
 	params->precision = d;
